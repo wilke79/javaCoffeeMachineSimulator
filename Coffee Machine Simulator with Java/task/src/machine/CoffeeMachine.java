f2ee6dd79d177d73ab98d2c1f68a322890d81ab3
wilke <wilke@gunt.de>
2024-07-16 16:01:40 +0200

    Calculating ingredients

--- Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
+++ Coffee Machine Simulator with Java/task/src/machine/CoffeeMachine.java
@@ -1,13 +1,26 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee");
-        System.out.println("Grinding coffee beans");
-        System.out.println("Boiling water");
-        System.out.println("Mixing boiled water with crushed coffee beans");
-        System.out.println("Pouring coffee into the cup");
-        System.out.println("Pouring some milk into the cup");
-        System.out.println("Coffee is ready!");
+        final int WATER_PER_CUP = 200;
+        final int MILK_PER_CUP = 50;
+        final int BEANS_PER_CUP = 15;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write how many cups of coffee you will need: ");
+        int cups = 0;
+        try {
+            cups = Integer.parseInt(scanner.nextLine());
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input!");
+        }
+        int water = cups * WATER_PER_CUP;
+        int milk = cups * MILK_PER_CUP;
+        int beans = cups * BEANS_PER_CUP;
+        System.out.printf("For %d cups of coffee you will need:\n", cups);
+        System.out.printf("%d ml of water\n", water);
+        System.out.printf("%d ml of milk\n", milk);
+        System.out.printf("%d ml of beans\n", beans);
     }
 }
